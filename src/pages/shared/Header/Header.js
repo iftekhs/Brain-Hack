1842b5f670ea82978f92fa8eac57f12301dc0fb7
iftekhs <iftekhstudio@gmail.com>
2022-10-25 16:14:58 +0600

    added dark mode toggle swithc button and sing in and sign up button

--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
 import './Header.css';
 
 const Header = () => {
@@ -13,7 +14,7 @@ const Header = () => {
             </h4>
           </div>
         </Link>
-        <div className="flex gap-4">
+        <div className="flex gap-4 items-center">
           <nav>
             <NavLink
               className={({ isActive }) =>
@@ -37,15 +38,16 @@ const Header = () => {
               Blog
             </NavLink>
           </nav>
-          <div>
-            <label
-              htmlFor="checked-toggle"
-              className="inline-flex relative items-center cursor-pointer">
-              <input type="checkbox" value="" id="checked-toggle" className="sr-only peer" />
-              <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            </label>
-            
-          </div>
+
+          <label
+            htmlFor="checked-toggle"
+            className="inline-flex relative items-center cursor-pointer">
+            <input type="checkbox" value="" id="checked-toggle" className="sr-only peer" />
+            <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+          </label>
+
+          <button className="btn bg-cpurple text-white hover:bg-violet-600">Sign In</button>
+          <button className="btn bg-cpurple text-white hover:bg-violet-600">Sign Up</button>
         </div>
       </div>
     </header>
