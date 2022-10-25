33b9117739a584c07430b328652ba0d6bf8f6a10
iftekhs <iftekhstudio@gmail.com>
2022-10-25 21:08:46 +0600

    created the auth provider component and the auth context

---
+++ src/contexts/AuthProvider.js
@@ -0,0 +1,77 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  sendEmailVerification,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from 'firebase/auth';
+import app from '../../firebase/firebase.config';
+
+export const AuthContext = createContext();
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  // --------------  Auth Functions --------------
+
+  const providerLogin = (provider) => {
+    return signInWithPopup(auth, provider);
+  };
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const updateUserProfile = (profile) => {
+    return updateProfile(auth.currentUser, profile);
+  };
+
+  const verifyEmail = () => {
+    return sendEmailVerification(auth.currentUser);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser === null || currentUser.emailVerified) {
+        setUser(currentUser);
+      }
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+  // -------------- Auth Functions  --------------
+
+  const authInfo = {
+    user,
+    loading,
+    providerLogin,
+    createUser,
+    signIn,
+    updateUserProfile,
+    logOut,
+    verifyEmail,
+    setLoading,
+  };
+
+  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
