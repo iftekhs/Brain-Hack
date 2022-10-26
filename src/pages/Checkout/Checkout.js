325f9e5e025aade3e52e514a8ded93942174fa85
iftekhs <iftekhstudio@gmail.com>
2022-10-26 20:16:29 +0600

    created the privateroute

--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Checkout = () => {
+  const course = useLoaderData();
   return (
     <section className="px-2 py-5">
       <div className="container mx-auto">
